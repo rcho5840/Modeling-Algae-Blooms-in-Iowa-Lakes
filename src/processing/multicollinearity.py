369ca2aa177ecd677df07d65f76892d0c667efff
Randy Cho <randycho2005@gmail.com>
2023-12-30 13:37:10 -0500

    Created the structure of my repo, began initial data exploration and processing

---
+++ src/processing/multicollinearity.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+def multicollinearity(df, threshold, target):
+    X = df.drop(target, axis = 1)
+    vif_data = pd.DataFrame()
+    vif_data["Feature"] = X.columns
+    vif_data["VIF"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+    
+    return print(vif_data), df.corr()
